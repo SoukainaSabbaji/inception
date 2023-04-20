cd9eee911b1b63efb518b88ce5b443595a10f904
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-04-20 21:30:32 +0000

    edited .sql file for mariadb and done with .env file hopefully

--- inception/srcs/requirements/wordpress/tools/install_script.sh
+++ inception/srcs/requirements/wordpress/tools/install_script.sh
@@ -10,9 +10,9 @@ fi
 sed -i 's/^listen = .*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 wp core download --path=/var/www/html --allow-root
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST" --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$WP_HOST" --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL"  --path=/var/www/html --skip-email
 wp user create $WP_USER $WP_USER_EMAIL  --user_pass=$WP_USER_PWD --role=author  --path=/var/www/html --allow-root
-bash
+php-fpm7.3 -F
 
 
