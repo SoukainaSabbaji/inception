c875f3ddbc728f0873660f53af0feeb845b098c7
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-04-24 13:58:00 -0400

    almost done with mariadb

--- inception/srcs/requirements/mariadb/tools/testdb.sh
+++ inception/srcs/requirements/mariadb/tools/testdb.sh
@@ -1,19 +1,7 @@
-#!/bin/bash
 
-service mysql start
-mysql_secure_installation <<EOF
-
-y
-hamid
-hamid
-y
-y
-y
-y
-EOF
-
-echo "CREATE DATABASE my_database;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON my_database.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-FLUSH PRIVILEGES;" | tr -d '\r' | mysql -u root -p${DB_PASSWORD}
-
+# !/bin/bash
+echo "CREATE DATABASE IF NOT EXISTS "${DB_NAME}";" > database.sql
+echo "CREATE USER IF NOT EXISTS "${DB_USER}" IDENTIFIED BY" "'${DB_PASS}';" >> database.sql
+echo "GRANT ALL PRIVILEGES ON "${DB_NAME}".* TO "${DB_USER}";" >> database.sql
+echo "ALTER USER" "'${DB_ROOT_USER'"@"'localhost'" "IDENTIFIED BY" "'${DB_ROOT_PASS}';" >> database.sql
+echo "FLUSH PRIVILEGES;" >> database.sql
