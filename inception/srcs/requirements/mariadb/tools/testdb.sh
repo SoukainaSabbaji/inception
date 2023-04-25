e42d595065bded49c7a3e849a0c08fec97f50b0a
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-04-25 10:14:32 -0400

    containers up but logs show errors bghit nmot

--- inception/srcs/requirements/mariadb/tools/testdb.sh
+++ inception/srcs/requirements/mariadb/tools/testdb.sh
@@ -1,7 +1,42 @@
+#!/bin/sh
 
-# !/bin/bash
-echo "CREATE DATABASE IF NOT EXISTS "${DB_NAME}";" > database.sql
-echo "CREATE USER IF NOT EXISTS "${DB_USER}" IDENTIFIED BY" "'${DB_PASS}';" >> database.sql
-echo "GRANT ALL PRIVILEGES ON "${DB_NAME}".* TO "${DB_USER}";" >> database.sql
-echo "ALTER USER" "'${DB_ROOT_USER'"@"'localhost'" "IDENTIFIED BY" "'${DB_ROOT_PASS}';" >> database.sql
-echo "FLUSH PRIVILEGES;" >> database.sql
+MYCNF="/etc/my.cnf.d/mariadb-server.cnf"
+
+if [ ! -d /run/mysqld ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql /run/mysqld
+fi
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+	chown -R mysql:mysql /var/lib/mysql
+
+	if ! command -v mysql_install_db &> /dev/null; then
+		echo "Error: mysql_install_db command not found"
+		exit 1
+	fi
+
+	if ! mysql_install_db --basedir=/usr --user=mysql --datadir=/var/lib/mysql --rpm > /dev/null; then
+		echo "Error: Failed to initialize MariaDB data directory"
+		exit 1
+	fi
+
+	sql=$(cat <<EOF
+USE mysql;
+FLUSH PRIVILEGES;
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
+CREATE DATABASE IF NOT EXISTS '$DB_NAME';
+CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+)
+
+	echo "$sql" | /usr/bin/mysqld --user=mysql --bootstrap
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" "$MYCNF"
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" "$MYCNF"
+/usr/bin/mysqld --user=mysql --console
