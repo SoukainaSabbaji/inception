54560f9f96d2c3caba9d251215511d0c01920003
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2023-04-28 13:00:23 +0100

    trying to ignore .env file

--- installhehe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ~/.zshrc ~/.zshrc.backup
-cp ./newzsh ~/.zshrc
-
-source ~/.zshrc
-
-echo "Done!"
-
